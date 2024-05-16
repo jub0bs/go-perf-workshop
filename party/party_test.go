16da802f344b7c438b16031ef0d4c0e52995d152
jub0bs <jcretel-infosec+github@protonmail.com>
2024-05-16 19:13:45 +0200

    add some benchmarks

--- party/party_test.go
+++ party/party_test.go
@@ -1,6 +1,7 @@
 package party_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/jub0bs/go-perf-workshop/party"
@@ -69,5 +70,39 @@ func TestBouncerCheck(t *testing.T) {
 			}
 		}
 		t.Run(c.desc, f)
+	}
+}
+
+func BenchmarkBouncerCheck(b *testing.B) {
+	type BenchCase struct {
+		desc   string
+		guests []string
+		csv    string
+	}
+	cases := []BenchCase{
+		{
+			desc:   "all guests",
+			guests: []string{"Foo", "Bar"},
+			csv:    "foo,bar",
+		}, {
+			desc:   "all guests but duplicated many times",
+			guests: []string{"Foo", "Bar"},
+			csv:    strings.Repeat("foo,bar", 1024),
+		}, {
+			desc: "maliciously long non-invited name",
+			// see https://en.wikipedia.org/wiki/Hubert_Blaine_Wolfeschlegelsteinhausenbergerdorff_Sr.
+			guests: []string{"Foo", "Bar"},
+			csv:    strings.Repeat("a", 1024),
+		},
+	}
+	for _, c := range cases {
+		f := func(b *testing.B) {
+			bouncer := party.NewBouncer(c.guests...)
+			b.ResetTimer()
+			for range b.N {
+				bouncer.Check(c.csv)
+			}
+		}
+		b.Run(c.desc, f)
 	}
 }
